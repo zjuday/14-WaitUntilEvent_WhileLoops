b46d128aa63926f385c0381b19f7dc7449452288
zjuday <judayzj@rose-hulman.edu>
2019-02-06 10:36:24 -0500

    DONE

--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -3,7 +3,7 @@ This module demonstrates lets you practice INPUT from the CONSOLE.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and Zachary Juday.
+         and Zachary Juday
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import math
